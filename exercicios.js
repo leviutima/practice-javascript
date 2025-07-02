fdc458552cd487c87ee937392cbb39f2b89c5819
leviutima <levi.utima@gmail.com>
2025-07-02 01:35:24 -0300

    feat: create confition if else

---
+++ exercicios.js
@@ -0,0 +1,12 @@
+const numero1 = 10;
+const numero2 = 2;
+const operacao = "multiplicação";
+
+if (operacao === "soma") {
+  console.log(numero1 + numero2);
+} else if (operacao === "multiplicação") {
+  console.log(numero1 * numero2);
+} else {
+  console.log("Operação inválida ou não identificada");
+}
+
